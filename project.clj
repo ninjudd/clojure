45a4c96d7dc0032c54296238787553bbd0812bc6
ninjudd <git@justinbalthrop.com>
2010-10-04 12:01:14 -0700

    use :aot-test to compile test source

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject clojure "1.3.0-SNAPSHOT"
   :description "The Clojure programming language"
   :aot [:exclude clojure.parallel]
+  :aot-test [clojure.test-clojure.protocols.examples clojure.test-clojure.genclass.examples]
   :main clojure.main
   :jar-classpath "."
   :jar-files [["src/clj/clojure/version.properties" "clojure/version.properties"]]
   :java-compile {:includejavaruntime true :target "1.5"})
-
