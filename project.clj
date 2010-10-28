5715d3472278f959b4f78ca5f9741544edadae7f
ninjudd <git@justinbalthrop.com>
2010-10-28 13:10:42 -0700

    use :aot-test

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject clojure "1.2.0"
+(defproject org.clojars.ninjudd/clojure "1.2.0"
   :description "The Clojure programming language"
   :aot [:exclude clojure.parallel]
+  :aot-test [clojure.test-clojure.protocols.examples clojure.test-clojure.genclass.examples]
   :main clojure.main
   :jar-classpath "."
   :jar-files [["src/clj/clojure/version.properties" "clojure/version.properties"]]
   :java-compile {:includejavaruntime true :target "1.5"})
-
